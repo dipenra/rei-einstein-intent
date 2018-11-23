0a170e5a4840287b1aa8d3a7d4265038c2f17647
Dipenra Rai <raidipenra@yahoo.com>
2018-11-23 10:11:57 -0800

    migrating over all the grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,27 +2,69 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-	  pkg: grunt.file.readJSON('package.json'),
-	  sass: {
-		dist: {
-			options: {
-				sourcemap: 'none',
-			},
-			files: [{
-				expand: true,
-				cwd: 'static/scss',
-				src: ['*.scss'],
-				dest: 'public/css',
-				ext: '.css'
-			}]
+		pkg: grunt.file.readJSON('package.json'),
+	  	clean: {
+			all: ['public/css/', 'public/js/']
+		},
+		jshint: {
+			all: ['Gruntfile.js', 'public/js/*.js']
+		},
+		sass: {
+			all: {
+				options: {
+					sourcemap: 'none',
+				},
+				files: [{
+					expand: true,
+					cwd: 'public/scss',
+					src: ['*.scss'],
+					dest: 'public/css',
+					ext: '.css'
+				}]
+			}
+	  	},
+		cssmin: {
+			all: {
+				files: [{
+					expand: true,
+					cwd: 'public/css',
+					src: ['*.css', '!*.min.css'],
+					dest: 'public/css',
+					ext: '.min.css'
+				}]
+			}
+		},
+		uglify: {
+			all: {
+				files: [{
+					expand: true,
+					src: ['public/js/*.js'],
+					dest: 'public/',
+					cwd: '.',
+					rename: function (dst, src) {
+						var file = src.replace('public/', '');
+						return dst + '/' + file.replace('.js', '.min.js');
+					}
+				}]
+			}
+		},
+		watch: {
+			all: {
+				files: ['public/scss/*.scss', 'public/js/*.js'],
+				tasks: ['default']
+			}
 		}
-	  }
 	});
-  
+	
 	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-  
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
 	// Default task(s).
-	grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['jshint','sass']);
   
   };
