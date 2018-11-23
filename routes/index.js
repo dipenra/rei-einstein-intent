048c409002f6f3467f9c75f42e14b598ce595d96
Dipenra Rai <raidipenra@yahoo.com>
2018-11-23 15:11:31 -0800

    title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'REI Einstein Search' });
 });
 
 module.exports = router;
