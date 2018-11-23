e303e13f1ca356d9e618ba4ca8995d707983f95e
Dipenra Rai <raidipenra@yahoo.com>
2018-11-23 15:10:50 -0800

    adding api to the route

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apiV1Router = require('./routes/api.v1');
 
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +27,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1', apiV1Router);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
